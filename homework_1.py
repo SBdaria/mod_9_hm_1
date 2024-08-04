06554c675b34f5befcaae8824d3adc40565d5d03
SBdaria <168875873+SBdaria@users.noreply.github.com>
2024-08-04 17:16:46 +0300

    Add files via upload

---
+++ homework_1.py
@@ -0,0 +1,8 @@
+def apply_all_func(int_list, *functions):
+    res = {}
+    for i in functions:
+        res.update({i.__name__ : i(int_list)})
+    return res
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
